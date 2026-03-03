75d457f9a6ffa6afc5dbd4bdd6066040ba8fca3e
sofyan <sofyanmeziane1@gmail.com>
2026-03-03 15:27:53 +0100

    le service config-server bien fonctionne

---
+++ services/config-server/src/test/java/com/gestionincidents/config_server/ConfigSreverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionincidents.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSreverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
